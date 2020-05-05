9de54f5bd8a4bb8a24ecca680b677fb1a7559b73
okamotchan <m.okamotchan@gmail.com>
2020-05-05 23:28:33 +0900

    first commit

---
+++ src/TodoApp.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TodoFooter } from './components/TodoFooter';
+import { TodoList } from './components/TodoList';
+
+export class TodoApp extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: {
+        '04': {
+          label: 'Todo 4',
+          completed: true
+        },
+        '03': {
+          label: 'Todo 3',
+          completed: false
+        },
+        '02': {
+          label: 'Todo 2',
+          completed: false
+        },
+        '01': {
+          label: 'Todo 1',
+          completed: false
+        }
+      },
+      filter: 'all'
+    };
+  }
+  render() {
+    const { filter, todos = [] } = this.state;
+    return (
+      <div>
+        <TodoList todos={todos} filter={filter} />
+        <TodoFooter todos={todos} />
+      </div>
+    );
+  }
+}
