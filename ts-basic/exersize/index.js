62f3e750819546c3a8ecaa7ee8d5cea3f5883f7f
okamotchan <m.okamotchan@gmail.com>
2020-05-06 14:03:03 +0900

    interface

--- ts-basic/exersize/index.js
+++ ts-basic/exersize/index.js
@@ -1,4 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var fib_1 = require("./fib");
+// fibonacchi
 console.log(fib_1.fib(fib_1["default"]));
+var myCar = {
+    wheels: 5,
+    color: 'red',
+    make: 'toyota',
+    model: 'prius'
+};
+console.log('mycar:', JSON.stringify(myCar));
--- ts-basic/exersize/index.ts
+++ ts-basic/exersize/index.ts
@@ -1,3 +1,25 @@
 import Fibconst, { fib } from './fib';
 
+// fibonacchi
 console.log(fib(Fibconst))
+
+// type & interface
+type TraficLight = 'red' | 'green' | 'yellow'
+
+interface Car {
+  wheels: number;
+  color: string;
+  make: string;
+  model: string;
+}
+
+const myCar: Car = {
+  wheels: 5,
+  color: 'red',
+  make: 'toyota',
+  model: 'prius'
+}
+
+console.log('mycar:', JSON.stringify(myCar))
+
+export {};
